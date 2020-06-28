ffe82f3df47f9a903d58befc4a088240eb2fea0f
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-28 22:10:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@
 def line(array)
   if array.length == 0 
     puts "The line is currently empty."
-  elsif
+  else new_array = array.each_with_index.map do |name, index| "#{index + 1}. #{name}"
   end
+  
 end 
