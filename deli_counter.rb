2006aeb7694ba4ec8ff3eb97eaa3466a32f69914
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-28 22:04:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 # Write your code here.
 
-def line(current_line)
-  if current_line == 0 
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli == 0 
     puts "The line is currently empty."
   end
 end 
